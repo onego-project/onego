ad85a2cf425194701c3e86361458ff1e3ab52b12
Lenka Svetlovska <lenocka369@gmail.com>
2018-08-08 11:28:33 +0200

    Add resizeRequest

---
+++ resizerequest/ResizeRequest.go
@@ -0,0 +1,25 @@
+package resizerequest
+
+import (
+	"fmt"
+	"github.com/owlet123/onego/blueprint"
+)
+
+// ResizeRequest struct
+type ResizeRequest struct {
+	CPU    float32
+	VCPU   int
+	Memory int
+}
+
+// Render method
+func (rr ResizeRequest) Render() string {
+	bpmap := map[string]blueprint.Node{
+		"cpu":    {Value: fmt.Sprintf("%.2f", rr.CPU)},
+		"vcpu":   {Value: fmt.Sprintf("%d", rr.VCPU)},
+		"memory": {Value: fmt.Sprintf("%d", rr.Memory)},
+	}
+	bp := blueprint.Blueprint{Values: bpmap, Root: "template"}
+
+	return bp.Render()
+}
