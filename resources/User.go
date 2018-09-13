dd04c14bc357cc1fca499ede4cc62494457619f1
Lenka <lenocka369@gmail.com>
2018-09-13 14:24:32 +0200

    Add User structure with getters and tests
    User structure represents User in OpenNebula and inherit XML data and methods from
    Resource structure.

---
+++ resources/User.go
@@ -0,0 +1,121 @@
+package resources
+
+import (
+	"fmt"
+	"github.com/beevik/etree"
+	"strconv"
+	"time"
+)
+
+// User structure represents user and inherits XML data and methods from Resource structure
+type User struct {
+	Resource
+}
+
+// LoginToken structure represents login token of given user with attributes token, expiration time and egid
+type LoginToken struct {
+	Token          string
+	ExpirationTime *time.Time
+	EGID           int
+}
+
+// CreateUserWithID constructs User with id
+func CreateUserWithID(id int) *User {
+	return &User{*CreateResource("USER", id)}
+}
+
+// CreateUserFromXML constructs User with full xml data
+func CreateUserFromXML(XMLdata *etree.Element) *User {
+	return &User{Resource: Resource{XMLData: XMLdata}}
+}
+
+// Password method returns password for the given user
+func (u *User) Password() (string, error) {
+	return u.Attribute("PASSWORD")
+}
+
+// AuthDriver method returns authentication driver for the given user
+func (u *User) AuthDriver() (string, error) {
+	return u.Attribute("AUTH_DRIVER")
+}
+
+// MainGroup method returns main group ID of the given user
+func (u *User) MainGroup() (int, error) {
+	return u.intAttribute("GID")
+}
+
+// Groups method returns list of groups with main group at index 0
+func (u *User) Groups() ([]int, error) {
+	elements := u.XMLData.FindElements("GROUPS/ID")
+	if len(elements) == 0 {
+		return nil, fmt.Errorf("no group")
+	}
+
+	groups := make([]int, len(elements))
+
+	for i, e := range elements {
+		id, err := strconv.Atoi(e.Text())
+		if err != nil {
+			return nil, err
+		}
+		groups[i] = id
+	}
+	return groups, nil
+}
+
+// Enabled returns true for enabled
+func (u *User) Enabled() (bool, error) {
+	ret, err := u.intAttribute("ENABLED")
+	if err != nil {
+		return false, err
+	}
+	return intToBool(ret), nil
+}
+
+// LoginTokens returns list of login tokens for the given user
+func (u *User) LoginTokens() ([]LoginToken, error) {
+	elements := u.XMLData.FindElements("LOGIN_TOKEN")
+	if len(elements) == 0 {
+		return make([]LoginToken, 0), nil
+	}
+
+	loginTokens := make([]LoginToken, len(elements))
+
+	for i, e := range elements {
+		tokenElement := e.FindElement("TOKEN")
+		if tokenElement == nil {
+			return nil, fmt.Errorf("no token in login token")
+		}
+
+		expTimeElement := e.FindElement("EXPIRATION_TIME")
+		if expTimeElement == nil {
+			return nil, fmt.Errorf("no expiration time in login token")
+		}
+
+		expTime, err := strconv.ParseInt(expTimeElement.Text(), base10, bitSize64)
+		if err != nil {
+			return nil, err
+		}
+		var expirationTime *time.Time
+
+		if expTime == -1 {
+			expirationTime = nil
+		} else {
+			expT := time.Unix(expTime, 0)
+			expirationTime = &expT
+		}
+
+		egidElement := e.FindElement("EGID")
+		if egidElement == nil {
+			return nil, fmt.Errorf("no egid in login token")
+		}
+
+		egid, err := strconv.Atoi(egidElement.Text())
+		if err != nil {
+			return nil, err
+		}
+
+		loginTokens[i] = LoginToken{Token: tokenElement.Text(), ExpirationTime: expirationTime, EGID: egid}
+	}
+	return loginTokens, nil
+}
---
+++ resources/User_test.go
@@ -0,0 +1,120 @@
+package resources
+
+import (
+	"github.com/beevik/etree"
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+	"time"
+)
+
+const (
+	userXML = "xml/user.xml"
+)
+
+var _ = ginkgo.Describe("User", func() {
+	var (
+		doc  *etree.Document
+		user *User
+		err  error
+	)
+
+	ginkgo.Describe("getters", func() {
+		var loginTokens []LoginToken
+
+		ginkgo.BeforeEach(func() {
+			// create user with data
+			doc = etree.NewDocument()
+			err = doc.ReadFromFile(userXML)
+			user = CreateUserFromXML(doc.Root())
+		})
+
+		ginkgo.It("should find all user attributes", func() {
+			gomega.Expect(err).Should(gomega.BeNil()) // no error during BeforeEach
+			gomega.Expect(user).ShouldNot(gomega.BeNil())
+
+			gomega.Expect(user.ID()).To(gomega.Equal(22))
+			gomega.Expect(user.Name()).To(gomega.Equal("Karol"))
+			gomega.Expect(user.Password()).To(gomega.Equal("6adfb183a4a2c94a2f92dab5ade762a47889a5a1"))
+			gomega.Expect(user.AuthDriver()).To(gomega.Equal("non-existing-driver"))
+			gomega.Expect(user.MainGroup()).ShouldNot(gomega.BeNil())
+			gomega.Expect(user.Groups()).To(gomega.HaveLen(1))
+			gomega.Expect(user.Enabled()).To(gomega.BeTrue())
+
+			loginTokens, err = user.LoginTokens()
+			gomega.Expect(err).Should(gomega.BeNil())
+			gomega.Expect(loginTokens).To(gomega.HaveLen(1))
+			gomega.Expect(loginTokens[0].EGID).To(gomega.Equal(-1))
+			expTime := time.Unix(int64(1497632279), 0)
+			gomega.Expect(loginTokens[0].ExpirationTime).To(gomega.Equal(&expTime))
+			gomega.Expect(loginTokens[0].Token).To(gomega.Equal("325efc70635143dd1b33a8d73d0a1aa159515dd2"))
+		})
+	})
+
+	ginkgo.Describe("create user", func() {
+		ginkgo.It("should create user", func() {
+			gomega.Expect(err).Should(gomega.BeNil()) // no error during BeforeEach
+
+			user = CreateUserWithID(42)
+
+			gomega.Expect(user.ID()).To(gomega.Equal(42))
+		})
+
+		ginkgo.Context("when user doesn't have given attribute", func() {
+			ginkgo.BeforeEach(func() {
+				user = CreateUserWithID(42)
+				err = nil
+			})
+
+			ginkgo.It("should return that user doesn't have name", func() {
+				gomega.Expect(err).Should(gomega.BeNil()) // no error during BeforeEach
+
+				_, err = user.Name()
+				gomega.Expect(err).ShouldNot(gomega.BeNil())
+			})
+
+			ginkgo.It("should return that user doesn't have password", func() {
+				gomega.Expect(err).Should(gomega.BeNil()) // no error during BeforeEach
+
+				_, err = user.Password()
+				gomega.Expect(err).ShouldNot(gomega.BeNil())
+			})
+
+			ginkgo.It("should return that user doesn't have AuthDriver", func() {
+				gomega.Expect(err).Should(gomega.BeNil()) // no error during BeforeEach
+
+				_, err = user.AuthDriver()
+				gomega.Expect(err).ShouldNot(gomega.BeNil())
+			})
+
+			ginkgo.It("should return that user doesn't have main group", func() {
+				gomega.Expect(err).Should(gomega.BeNil()) // no error during BeforeEach
+
+				_, err = user.MainGroup()
+				gomega.Expect(err).ShouldNot(gomega.BeNil())
+			})
+
+			ginkgo.It("should return that user doesn't have groups", func() {
+				gomega.Expect(err).Should(gomega.BeNil()) // no error during BeforeEach
+
+				_, err = user.Groups()
+				gomega.Expect(err).ShouldNot(gomega.BeNil())
+			})
+
+			ginkgo.It("should return that user doesn't have Enabled", func() {
+				gomega.Expect(err).Should(gomega.BeNil()) // no error during BeforeEach
+
+				_, err = user.Enabled()
+				gomega.Expect(err).ShouldNot(gomega.BeNil())
+			})
+
+			ginkgo.It("should return empty array of LoginTokens and no error", func() {
+				gomega.Expect(err).Should(gomega.BeNil()) // no error during BeforeEach
+
+				var loginTokens []LoginToken
+				loginTokens, err = user.LoginTokens()
+				gomega.Expect(err).Should(gomega.BeNil())
+				gomega.Expect(loginTokens).Should(gomega.HaveLen(0))
+			})
+		})
+	})
+})
